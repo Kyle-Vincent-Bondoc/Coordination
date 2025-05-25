09f7ea1f919b22207f9b8e6f7043c2c7970199b2
Kyle-Vincent-Bondoc <kylevincentbondoc@gmail.com>
2025-05-26 00:06:27 +0800

    Create randomauto.py

---
+++ randomauto.py
@@ -0,0 +1,17 @@
+import random
+import time
+from config import *
+
+def random_move(moves):
+  move = random.choice(moves)
+  move()
+
+def automoverandom(moves, interval=MODE_INTERVAL, extra)
+    print("Starting automatic random movement every ", interval, " seconds.")
+    try:
+        while True:
+            random_move(moves)
+            print(extra)
+            time.sleep(interval)
+    except KeyboardInterrupt:
+        print("\nAuto movement stopped by user.")
